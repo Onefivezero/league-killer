b5e4495f5861fe07fb252194fe207c800a3b52ff
Mustafa Sönmez <43936380+Onefivezero@users.noreply.github.com>
2023-06-13 22:44:49 +0300

    Add while loop for periodic detection

--- league_killer.py
+++ league_killer.py
@@ -1,17 +1,20 @@
+import time
 import wmi
 
 c = wmi.WMI()
-
 banned_process_prefixes = ("leagueclient", "riotclient")
 
-league_related_processes = [
-    process for process in c.Win32_Process()
-    if process.name.lower().replace(" ", "").startswith(banned_process_prefixes)
-]
+while True:
+    league_related_processes = [
+        process for process in c.Win32_Process()
+        if process.name.lower().replace(" ", "").startswith(banned_process_prefixes)
+    ]
 
-for process in league_related_processes:
-    try:
-        process.Terminate()
-        print(f"{process.name} terminated.")
-    except Exception as e:
-        print(e)
+    for process in league_related_processes:
+        try:
+            process.Terminate()
+            print(f"{process.name} terminated.")
+        except Exception as e:
+            print(e)
+    
+    time.sleep(5)
